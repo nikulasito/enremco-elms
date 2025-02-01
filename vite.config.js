3b9e7d01c6e5cedfe0fddecb1d572640076237d0
Nico <n.alumbro@gmail.com>
2025-02-01 17:18:46 +0800

    Vite CSS

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'], // Add your CSS and JS files
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/style.css'], // Add your CSS and JS files
             refresh: true,
         }),
     ],
